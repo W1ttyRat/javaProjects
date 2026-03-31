b7d1f30818ca7071f9fa63ff1abdc695ecde34de
Tanel Metshein <tanemets@tlu.ee>
2026-03-31 14:46:59 +0300

    Change main method access modifier to public in CalculatorApp and TemperatureConverterApp

--- src/com/example/projects/beginner/converter/TemperatureConverterApp.java
+++ src/com/example/projects/beginner/converter/TemperatureConverterApp.java
@@ -3,7 +3,7 @@ package com.example.projects.beginner.converter;
 import java.util.Scanner;
 
 public class TemperatureConverterApp {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("--- Temperature Converter ---");
         Scanner sc = new Scanner(System.in);
         /*
